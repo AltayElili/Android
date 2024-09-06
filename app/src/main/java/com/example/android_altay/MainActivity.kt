389ff273b718eb0ecb5be85ceb2e0b5c8da66f8b
Altay Elili <altayalili28@gmail.com>
2024-09-06 22:18:04 +0400

    first_Commit

--- app/src/main/java/com/example/android_altay/MainActivity.kt
+++ app/src/main/java/com/example/android_altay/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.layout2)
+        setContentView(R.layout.layout7)
     }
 }
